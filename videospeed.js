0d7a4035696909042318342f7634838e092b4186
Mysterious <83178906+nostalgickd@users.noreply.github.com>
2021-11-14 22:00:10 -0300

    Update videospeed.js

--- videospeed.js
+++ videospeed.js
@@ -2,76 +2,69 @@ let
 titles= ["Normal","1.5x","2.0x","Custom"],
 urls= ["javascript:(function()%7Bdocument.querySelector(%22video%22).playbackRate%3D1%3B%7D)()%3B","javascript:(function()%7Bdocument.querySelector(%22video%22).playbackRate%3D1.5%3B%7D)()%3B","javascript:(function()%7Bdocument.querySelector(%22video%22).playbackRate%3D2%3B%7D)()%3B","javascript:(function()%7Blet%20a%3D%20prompt(%22Enter%20desired%20speed%20e.g%202.5%20or%204%22)%3B%0Adocument.querySelector(%22video%22).playbackRate%3D%20a%3B%7D)()%3B"];
 
-let create= (x)=> document.createElement(x),
-select= (x,y=document)=> y.querySelector(x),
-selectAll= (x,y=document)=> y.querySelectorAll(x);
-  
-let div= create("div"); div.id= "kdbmlistdiv";
-let present= select("#kdbmlistdiv");
-if(present){
-present.remove();
-document.body.append(div);	
-}
-else document.body.append(div);	
+let template= document.createElement("template");
+template.innerHTML=`
+<style>
+*{box-sizing: border-box; z-index: 9999999;}
 
-let regex= /^javascript.+/;
-titles.forEach((i,x)=>{
-let a= create("a"); div.append(a);
-a.text= i; a.className= "kdbmlista";
-if(regex.test(urls[x])){
-a.onclick=()=> location= urls[x];
-}
-else if(urls[x]) a.href= urls[x];
-});
-
-let closeicon= create("span"); div.prepend(closeicon);
-closeicon.className= "kdbmlistclose";
-closeicon.innerHTML= `<svg viewBox="0 0 24 24">
-<path d="M 2 2 L 22 22 M 2 22 L22 2" /></svg>`;
-
-closeicon.onclick=()=>{
-selectAll(".kdbmlista").forEach(i=> i.classList.toggle("closed"));
-};
-
-let style= create("style"); document.body.append(style);
-style.innerHTML=`
-#kdbmlistdiv{
-z-index:99999;
+div{
 position: fixed; left:1px; top:2px;
 display:flex; flex-direction: column;
 }
 
-.kdbmlista{
-box-sizing: border-box;
-height:1.5em; display: flex;
-align-items: center;
-padding: 2px 5px 2px 5px; 
+a{
+text-align: left;
+font: 15px "Arial";
+height: 1.5em; padding: 2px 5px; 
 background: white; color: black;
-text-decoration: none;
 border: 1px solid black;
+text-decoration: none;
 transition: .3s linear;
 margin-bottom: -1px;
-font: 15px "Arial";
-opacity: 1;
-pointer-events: auto;
 }
 
-.kdbmlista:hover{ background:yellow;}
+a:hover{background: yellow;}
+a.closed{opacity: 0; pointer-events: none;}
+span{height: 20px; width: 20px;}
+svg{stroke: red; stroke-width: 2.5; border: 1.5px solid red;}
 
-.kdbmlistclose{
-height: 2.5ch; width: 2.5ch;
-display:block;
-background: white;
+</style>
+<div>
+<span><svg viewBox="0 0 24 24"><path d="M 2 2 L 22 22 M 2 22 L22 2"/></svg></span>
+</div>`;
+
+
+if (!customElements.get("kd-list")){
+customElements.define("kd-list",
+class extends HTMLElement{
+constructor(){
+super();
+this.attachShadow({mode:"open"});
+}			
+})
 }
 
-.kdbmlistclose svg{
-width: 100%; height: 100%;
-stroke: red; stroke-width: 2;
-border: 1px solid red;
-stroke-linecap: round;
-}
 
-.kdbmlista.closed{
-opacity: 0;
-pointer-events: none;
-}`;
+let a= document.createElement("kd-list");
+let b= document.querySelector("kd-list");
+a.shadowRoot.append(template.content.cloneNode(true));
+
+if(b){
+b.remove();
+document.body.append(a);
+}
+else document.body.append(a);
+
+let regex= /^javascript.+/;
+titles.forEach((i,x)=>{
+let link= document.createElement("a"); link.text= i; 
+a.shadowRoot.querySelector("div").append(link);
+if(regex.test(urls[x])){
+link.onclick=()=> location= urls[x];
+}
+else if(urls[x]) link.href= urls[x];
+});
+
+a.shadowRoot.querySelector("span").onclick=()=>{
+a.shadowRoot.querySelectorAll("a").forEach(i=> i.classList.toggle("closed"));
+};
