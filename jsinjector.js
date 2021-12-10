7378104b1f692afe75edb3a8587ed969ed2ff8a7
Mysterious <83178906+nostalgickd@users.noreply.github.com>
2021-12-10 19:36:01 -0300

    JS Injector

--- jsinjector.js
+++ jsinjector.js
@@ -64,7 +64,7 @@ background: black;
 </style>
 <div id="container">
 <svg class="close" viewbox="0 0 40 40"><path d="M 10,10 L 30,30 M 30,10 L 10,30"/></svg>
-<textarea></textarea>
+<textarea spellcheck="false" autocapitalize="none" autocomplete="off"></textarea>
 <span class="run">RUN</span>
 </div>`;
 
